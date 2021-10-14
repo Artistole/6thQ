7a0ad9d962f92b52c6f514872fc86c90ce72a5f3
Naga Satya Geeth <67518197+Artistole@users.noreply.github.com>
2021-10-14 13:56:21 +0000

    refactor added comments

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var readLineSync = require('readline-sync');
 
-var name = [];
+var name = [];          //defined arrays
 var uni = [];
 var pre = [];
 var final = [];
 
+
+//created a for loop to ask for 5 students
 for (i=0;i<5;i++)
 {
-var names = readLineSync.question("what is your name? ");
-name.push(names);
+var names = readLineSync.question("what is your name? ");  //used a different variable to take input
+name.push(names); //used .push to push the value into array that we got from the variable
 var unis  = readLineSync.question("unit test marks? ");
 uni.push(unis);
 var pres = readLineSync.question("prefinal marks? ");
@@ -17,9 +19,9 @@ var finals = readLineSync.question("final marks? ");
 final.push(finals);
 }
 
-var maxuni = uni[0];
-var maxpre = pre[0];
-var maxfin = final[0];
+var maxuni = uni[0]; //maxuni taken to calculate the highest marks
+var maxpre = pre[0];  //taking them initiall with index 0 can help
+var maxfin = final[0]; // in comparing with other ele using for loop
 var addU = 0;
 var addP = 0;
 var addF = 0;
@@ -27,12 +29,12 @@ var j = 0;
 var k=0;
 var l=0;
 
-for(i=0;i<5;i++)
+for(i=0;i<5;i++)     //for loop for calculating the highest marks and name
 {
-  if(uni[i]>maxuni)
+  if(uni[i]>maxuni)  //if loop to calculate highest marks
   {
     maxuni=uni[i]
-    j=i
+    j=i              //variable j for finding out the corresponding name of the highest marks
 
   }
 
@@ -56,7 +58,7 @@ for(i=0;i<5;i++)
   addF = addF + parseInt(final[i])
 }
 
-avgU = addU/uni.length
+avgU = addU/uni.length    //defined new variables to calculate the avg of the marks
 avgP = addP/pre.length
 avgF = addF/final.length
 
